901e376d55394386aa2acbc31f16d2be185b2d95
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2023-08-18 20:56:15 -0300

    versão testada em linux

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public static class Constants
 {
-    public const float FEEDBACK_COLLISION_SIDEWALK = -5f;
-    public const float FEEDBACK_COLLISION_WALL = -5f;
+    public const float FEEDBACK_COLLISION_SIDEWALK = -2f;
+    public const float FEEDBACK_COLLISION_WALL = -2f;
     public const float FEEDBACK_MAXSTEPS_REACHED = -5f;
-    public const float FEEDBACK_CAR_UPSIDE_DOWN = -5f;
-    public const float FEEDBACK_CHECKPOINT_REACHED = 1f;
+    public const float FEEDBACK_CAR_UPSIDE_DOWN = -15f;
+    public const float FEEDBACK_CHECKPOINT_REACHED = 2f;
     public const float FEEDBACK_DESTINATION_REACHED = 5f;
 
 }
