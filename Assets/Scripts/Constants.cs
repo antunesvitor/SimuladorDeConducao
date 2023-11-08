501e85e00dcf1ac2f57d421e77ed6e5c240d9add
Vitor Guilherme Antunes <vguilhermeantunes@gmail.com>
2023-11-08 20:21:53 -0300

    sucesso ao treinar o modelo 2.0 no desafio GERAL

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -8,9 +8,9 @@ public static class Constants
     public const float FEEDBACK_COLLISION_SIDEWALK = -.05f;
     public const float FEEDBACK_COLLISION_WALL = -.05f;
     public const float FEEDBACK_MAXSTEPS_REACHED = -0f;                    //Esta é aplicada quando ele chega ao final do EP e não cumpriu o objetivo
-    public const float FEEDBACK_MAXSTEPS_REACHED_PER_STEP = -.3f;             //Esta é aplicada por step, pouco a pouco dentro do episódio
+    public const float FEEDBACK_MAXSTEPS_REACHED_PER_STEP = -.7f;             //Esta é aplicada por step, pouco a pouco dentro do episódio
     public const float FEEDBACK_CAR_UPSIDE_DOWN = -1f;
-    public const float FEEDBACK_MAX_CHECKPOINT_REACHED = .5f;
-    public const float FEEDBACK_DESTINATION_REACHED = .5f;
+    public const float FEEDBACK_MAX_CHECKPOINT_REACHED = .8f;
+    public const float FEEDBACK_DESTINATION_REACHED = .2f;
 
 }
